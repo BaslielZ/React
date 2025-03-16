87d55c90693266f904672d6117dc80b1f4ea314b
Basliel <baslielz727@gmail.com>
2025-03-16 14:31:32 +0200

    card component

--- myReactApp/src/App.jsx
+++ myReactApp/src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css'
-import React from 'react';
+import React, { useState } from 'react';
+
+const Card =({title})=>{
+
+  const [hasLiked, setHasLiked] = useState(false);
+
+  return(
+    <div className='card'>
+      <h2>{title}</h2>
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? 'Liked': 'Like'}</button>
+    </div>
+  )
+}
 
 const App = () =>{
   
-  return (<div>
-    <h1>Hello World</h1>
+  return (
+  <div className='card-container'>
+    <Card title='The Lion King'/>
+    <Card title='Puss In Boots'/>
+    <Card title='Turning Red'/>
   </div>
-)
+  )
 }
 
 export default App
