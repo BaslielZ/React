07282432ad0057b8b1b6f5133254810f4b1785de
Basliel <baslielz727@gmail.com>
2025-03-23 22:21:49 +0200

    minor issues

--- myReactApp/src/App.jsx
+++ myReactApp/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Search from './components/Search';
 
 const APT_BASE_URL = 'https://api.themoviedb.org/3'
-const API_KEY = imprort.meta.env.VITE_TMDB_API_KEY
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY
 
 const API_OPTIONS = {
   method: 'GET',
@@ -14,15 +14,11 @@ const API_OPTIONS = {
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("")
-
-  useEffect(() => {
-    
-  }, []);
   return(
     <div className='pattern'>
       <div className='wrapper'>
         <header>
-          <img src="../public/hero-img.png" alt="Hero Banner" />
+          <img src="/hero-img.png" alt="Hero Banner" />
           <h1> <span className='text-gradient'>Find Movies</span> You'll Love</h1>
           </header>
       </div>
