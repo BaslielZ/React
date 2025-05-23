7952a7f6f6f265f857c3ec379f8040490c720149
Basliel <baslielz727@gmail.com>
2025-05-23 12:05:54 +0300

    installing vite updates

--- myReactApp/src/App.jsx
+++ myReactApp/src/App.jsx
@@ -5,7 +5,7 @@ import Spinner from './components/Spinner'
 
 const APT_BASE_URL = 'https://api.themoviedb.org/3'
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY
-
+ 
 const API_OPTIONS = {
   method: 'GET',
   headers:{
